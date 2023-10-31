0228fb895ece32798ba24b8e4979d22afd52d016
Andreea <Andreea@DESKTOP-1U5DEAK>
2023-10-31 11:14:55 +0200

    Add project files.

---
+++ Data/Nicolaie_Andreea_Lab2Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Nicolaie_Andreea_Lab2.Models;
+
+namespace Nicolaie_Andreea_Lab2.Data
+{
+    public class Nicolaie_Andreea_Lab2Context : DbContext
+    {
+        public Nicolaie_Andreea_Lab2Context (DbContextOptions<Nicolaie_Andreea_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Nicolaie_Andreea_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Nicolaie_Andreea_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Nicolaie_Andreea_Lab2.Models.Author>? Author { get; set; }
+
+        public DbSet<Nicolaie_Andreea_Lab2.Models.Category>? Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Nicolaie_Andreea_Lab2.Data;
+using Nicolaie_Andreea_Lab2.Models;
+
+namespace Nicolaie_Andreea_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Nicolaie_Andreea_Lab2.Data.Nicolaie_Andreea_Lab2Context _context;
+
+        public IndexModel(Nicolaie_Andreea_Lab2.Data.Nicolaie_Andreea_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get; set; } = default!;
+        public BookData BookD { get; set; }
+        public int BookID { get; set; }
+        public int CategoryID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? categoryID)
+            {
+            BookD = new BookData();
+            BookD.Books = await _context.Book.Include(b => b.Publisher).Include(b => b.BookCategories).ThenInclude(b => b.Category).AsNoTracking().OrderBy(b => b.Title).ToListAsync();
+
+            if (id != null)
+            {
+                BookID = id.Value;
+                Book book = BookD.Books
+                    .Where(i => i.ID == id.Value).Single();
+                BookD.Categories = book.BookCategories.Select(s => s.Category);
+
+            }
+        }
+    }
+}
